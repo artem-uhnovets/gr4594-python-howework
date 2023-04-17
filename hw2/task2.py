11c2ff6ba95fb77e0c9261d018609fd5b77ee909
ArtemYU <uhnovets@gmail.com>
2023-04-17 16:53:39 +0300

    add some solutions for task2/hw2

--- hw2/task2.py
+++ hw2/task2.py
@@ -6,8 +6,8 @@
 # Помогите Кате отгадать задуманные Петей числа.
 # 5, 6 -> 2, 3
 
-# s = a + b 
-# p = a * b 
+# s = a + b
+# p = a * b
 
 # a = s - b
 # a = p / b
@@ -36,3 +36,41 @@ elif a_2 + b_2 == s and a_2 * b_2 == p:
     print(f'{s}, {p} -> {a_2}, {b_2}')
 else:
     print('s and p are incompatible')
+
+""" 
+еще примеры решения
+print ('Введите сумму чисел S: ')
+s = int(input())
+print ('Введите произведение чисел P: ')
+p = int(input()) 
+for x in range (s):
+    for y in range (p):
+        if s == x + y  and p == x*y:
+            print (x, y)
+        # else:
+        #     print ('Введены не верные S и P')
+
+summa = int(input(f"Введите сумму чисел S = "))
+mult = int(input(f"Введите произведение чисел P = "))
+flag_break = False
+
+
+for x in range(summa):
+    if(flag_break == False):
+        for y in range(summa):
+            if (x + y == summa) and (x * y == mult):
+                print("загаданные числа:")
+                print(x, y)
+                flag_break = True
+                break
+            elif (x == summa -1) and (y == summa -1):
+                print("нет соответствующих чисел")
+
+                
+S = int(input("Input a sum of numbers: "))
+P = int(input("Input a multiple of numbers: "))
+x = 1
+while S - x != P/x:
+    x += 1
+print(f"First num is {x}, second num is {S - x}")
+"""
