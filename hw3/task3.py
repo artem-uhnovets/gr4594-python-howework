286c17138cab3f9e4088330e3241156051cf0c50
ArtemYU <uhnovets@gmail.com>
2023-04-17 09:42:00 +0300

    add 2nd solution of hw3/sem3

--- hw3/task3.py
+++ hw3/task3.py
@@ -25,35 +25,49 @@
 Ввод: ноутбук
 Вывод: 12
 '''
-word = input('Enter a word: ')
-word = word.upper()
+
+word = input('Enter a word: \n').upper()
 result = 0
-list = []
-for i in range(len(word)):
-    list.append(word[i])
-dict_pts = {
-    '0': 1,
-    '1': 2,
-    '2': 3,
-    '3': 4,
-    '4': 5,
-    '5': 8,
-    '6': 10
+""" 1 вариант решения """
+# dict_pts = {
+#     '0': 1,
+#     '1': 2,
+#     '2': 3,
+#     '3': 4,
+#     '4': 5,
+#     '5': 8,
+#     '6': 10
+# }
+# dict_letter = {
+#     '0': ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S','T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'],
+#     '1': ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'],
+#     '2': ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'],
+#     '3': ['F', 'H', 'V', 'W', 'Y', 'Й, Ы'],
+#     '4': ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч'],
+#     '5': ['J', 'X', 'Ш', 'Э', 'Ю'],
+#     '6': ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+# }
+# list_letter = []
+# for i in dict_letter.values():
+#     list_letter.append(i)
+# for i in range(len(word)):
+#     for j in range(len(list_letter)):
+#         if word[i] in list_letter[j]:
+#             result += dict_pts[str(j)]
+# print(result)
+""" 2 вариант решения """
+dict = {
+    ('A', 'E', 'I', 'O', 'U', 'L', 'N', 'S','T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'): 1,
+    ('D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'): 2,
+    ('B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'): 3,
+    ('F', 'H', 'V', 'W', 'Y', 'Й, Ы'): 4,
+    ('K', 'Ж', 'З', 'Х', 'Ц', 'Ч'): 5,
+    ('J', 'X', 'Ш', 'Э', 'Ю'): 8,
+    ('Q', 'Z', 'Ф', 'Щ', 'Ъ'): 10
 }
-dict_letter = {
-    '0': ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S','T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'],
-    '1': ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'],
-    '2': ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'],
-    '3': ['F', 'H', 'V', 'W', 'Y', 'Й, Ы'],
-    '4': ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч'],
-    '5': ['J', 'X', 'Ш', 'Э', 'Ю'],
-    '6': ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
-}
-list_letter = []
-for i in dict_letter.values():
-    list_letter.append(i)
 for i in range(len(word)):
-    for j in range(len(list_letter)):
-        if word[i] in list_letter[j]:
-            result += dict_pts[str(j)]
+    for j in dict.keys():
+        if word[i] in j:
+            result += dict[j]
+            break
 print(result)
