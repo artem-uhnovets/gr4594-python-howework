8d70874568fd07fd5513a424f1c16e454dd02b61
ArtemYU <uhnovets@gmail.com>
2023-04-18 21:20:31 +0300

    some little changes

--- hw4/task2.py
+++ hw4/task2.py
@@ -16,15 +16,15 @@
 import random
 max_summ = 0
 max_index = 0
-# list = [0] * int(input('Enter a number of blueberry bushes: '))
-# for i in range(len(list)):
-#     list[i] = random.randint(0, 9)
-# print(*list)
-# for i in range(len(list)):
-#     if (list[i-1] + list[i] + list[1 - len(list) + i]) > max_summ:
-#         max_summ = list[i-1] + list[i] + list[1 - len(list) + i]
-#         max_index = i
-# print(f'Max number of berries is {max_summ} and located in the bushes {len(list) if max_index == 0 else max_index}-{max_index + 1}-{max_index + 2 if (max_index + 2) <= len(list) else 1}')
+list = [0] * int(input('Enter a number of blueberry bushes: '))
+for i in range(len(list)):
+    list[i] = random.randint(0, 9)
+print(*list)
+for i in range(len(list)):
+    if (list[i-1] + list[i] + list[1 - len(list) + i]) > max_summ:
+        max_summ = list[i-1] + list[i] + list[1 - len(list) + i]
+        max_index = i
+print(f'Max number of berries is {max_summ} and located in the bushes {len(list) if max_index == 0 else max_index}-{max_index + 1}-{max_index + 2 if (max_index + 2) <= len(list) else 1}')
 
 # for i in range(len(list := [0] * int(input('Enter a number of blueberry bushes: ')))):
 #     list[i] = random.randint(0, 9)
